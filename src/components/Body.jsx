56cdffed899ca01def6d8546d4ee4e345f1ffc6c
Sooraj2003 <96384011+Sooraj2003@users.noreply.github.com>
2025-03-26 19:12:35 +0530

    made a body component

---
+++ src/components/Body.jsx
@@ -0,0 +1,43 @@
+import { Outlet, useNavigate } from "react-router-dom";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import { BASE_URL } from "../utils/constants";
+import axios from "axios";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "../utils/userSlice";
+
+const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((store) => store.user);
+
+  const fetchUser = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/profile", {
+        withCredentials: true,
+      });
+      dispatch(addUser(res?.data?.user));
+      console.log(res?.data?.user);
+    } catch (err) {
+      navigate("/login");
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    if (!user) {
+      fetchUser();
+    }
+  }, [user]); // Dependency added
+
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+export default Body;
