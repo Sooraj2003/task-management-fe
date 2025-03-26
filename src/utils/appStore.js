928e77ed7e4e0deea7a3f54236578af07a161aee
Sooraj2003 <96384011+Sooraj2003@users.noreply.github.com>
2025-03-26 19:14:57 +0530

    configured the slices into the appStore

---
+++ src/utils/appStore.js
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit";
+import userSlice from "./userSlice";
+import tasksSlice from "./tasksSlice"
+
+
+
+const appStore = configureStore({
+    reducer:{
+    user:userSlice,
+    tasks:tasksSlice
+
+    }
+})
+
+export default appStore;
