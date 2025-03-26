7e32019579abda7e9fda9266bab9b32265aeb4e2
Sooraj2003 <96384011+Sooraj2003@users.noreply.github.com>
2025-03-26 19:13:10 +0530

    made a navbar component for navigation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,36 @@
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { removeUser } from "../utils/userSlice";
+import { BASE_URL } from "../utils/constants";
 
 const Navbar = () => {
+  const role = import.meta.env.VITE_ROLE; // Use import.meta.env 
+  const user = useSelector(store=>store?.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = async()=>{
+    try{
+      await axios.post(BASE_URL+"/logout",{},{withCredentials:true});
+      dispatch(removeUser());
+      navigate("/login")
+
+    }catch(err){
+      console.error(err);
+    }
+    
+  }
   return (
     <div className="navbar bg-base-300 rounded-lg p-4">
     <div className="flex-1">
-      <a className="btn btn-ghost text-xl">tasks. 📔</a>
+      <Link  to="/" className="btn btn-ghost text-xl">tasks. 📔</Link>
     </div>
+    <p className="text-xs mx-2 md:text-lg ">{user && "Welcome, "+user?.firstName}</p>
     <div className="flex-none gap-2">
-      <div className="dropdown dropdown-end">
+    {user && <div className="dropdown dropdown-end">
         <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-          <div className="w-10 rounded-full">
+           <div className="w-10 rounded-full">
             <img
               alt="Tailwind CSS Navbar component"
               src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
@@ -17,16 +39,15 @@ const Navbar = () => {
         <ul
           tabIndex={0}
           className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-          <li>
-            <a className="justify-between">
-              Profile
-              <span className="badge">New</span>
-            </a>
+          {user?.firstName!==role && <li>
+          <Link to="/createTask" className="justify-between">
+              Create Task
+            </Link>
           </li>
-          <li><a>Settings</a></li>
-          <li><a>Logout</a></li>
+          }       
+          <li><a onClick={handleLogout}>Logout</a></li>
         </ul>
-      </div>
+      </div>}
     </div>
   </div>
   )
