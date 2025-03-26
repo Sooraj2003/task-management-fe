1404cadd62018dfad42fbe552680c6222618ede6
Sooraj2003 <96384011+Sooraj2003@users.noreply.github.com>
2025-03-26 19:12:10 +0530

    wrote code for client side routing using react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
-import Navbar from "./components/Navbar"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Login from "./components/Login"
+import Body from "./components/Body"
+import Tasks from "./components/Tasks"
+import CreateTask from "./components/CreateTask"
+import { useSelector } from "react-redux"
+import AdminTasks from "./components/AdminTasks"
+
 
 
 function App() {
+  const role = import.meta.env.VITE_ROLE; // Use import.meta.env 
+  const user = useSelector((store)=>store?.user);
+
+
 
   return (
+  
     <>
-      <Navbar/>  
+      <BrowserRouter>
+      <Routes>
+       <Route path="/" element={<Body/>}>
+       {(user?.firstName=== role )?<Route path="/" element={<AdminTasks/>}/>:<Route path="/" element={<Tasks/>}/> }
+        
+        <Route path="login" element={<Login/>}/>
+        <Route path="createTask" element={<CreateTask/>}/>
+       </Route>
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
